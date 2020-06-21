0e0869d1cadca91fc9ab4c8651716cdca2cfa1c1
ndorazil <nathandorazil@live.com>
2020-06-21 14:56:48 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,30 @@
+require 'pry'
+
 class Dog
     attr_accessor :name
 
     @@all = []
 
+    def save
+        @@all << self
+    end
+
     def initialize(name)
         @name = name
-        @@all << self
+        self.save
     end
 
     def self.all
         @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
     end
 end
