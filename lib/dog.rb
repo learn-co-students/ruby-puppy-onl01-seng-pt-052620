1192148f53920c7b2b498f81a6ed6cea4b487003
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-23 20:31:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name
+  @@all = []
+  @@all_names = []
+
+  def initialize(name)
+    @name = name
+    @@all_names << name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+    @@all_names = []
+  end
+
+  def self.print_all
+    puts @@all_names
+  end
+
+  def save
+    @@all << self
+  end
+end
